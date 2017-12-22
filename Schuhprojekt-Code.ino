54387d9ac9d037f168dbc4e729faa55368ff495c
Spider-S001 <34767626+Spider-S001@users.noreply.github.com>
2017-12-22 10:58:45 +0100

    Update Schuhprojekt-Code.ino

--- Schuhprojekt-Code.ino
+++ Schuhprojekt-Code.ino
@@ -28,10 +28,10 @@ void loop() {
       colorWipe(strip.Color(255, 0, 0), 50, 2);
       break;
     case 3:
-      rainbow(20);
+      rainbow(20, 3);
       break;
     case 4:
-      rainbowCycle(20);
+      rainbowCycle(20, 2);
       break;
   }
 }
@@ -64,8 +64,8 @@ void wheelstart() {
   colorWipe(strip.Color(255, 0, 0), 50, 0); // Red
   colorWipe(strip.Color(0, 255, 0), 50, 1); // Green
   colorWipe(strip.Color(0, 0, 255), 50, 2); // Blue
-  rainbow(20);
-  rainbowCycle(20);
+  rainbow(20, 3);
+  rainbowCycle(20, 2);
 }
 
 // Fill the dots one after the other with a color
@@ -82,18 +82,18 @@ void colorWipe(uint32_t c, uint8_t wait, int stat) {
   }
 }
 
-void rainbow(uint8_t wait) {
+void rainbow(uint8_t wait, uint8_t fnumber) {
   uint16_t i, j;
 
   for(j=0; j<256; j++) {
     for(i=0; i<strip.numPixels(); i++) {
       strip.setPixelColor(i, Wheel((i+j) & 255));
-      if(state != 3){
+      if(state != fnumber){
         break;
       }
     }
     strip.show();
-    if(state != 3){
+    if(state != fnumber){
       break;
     }
     delay(wait);
@@ -101,18 +101,18 @@ void rainbow(uint8_t wait) {
 }
 
 // Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
+void rainbowCycle(uint8_t wait, uint8_t fnumber) {
   uint16_t i, j;
 
   for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
     for(i=0; i< strip.numPixels(); i++) {
       strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-      if(state != 2){
+      if(state != fnumber){
         exit;
       }
     }
     strip.show();
-    if(state != 2){
+    if(state != fnumber){
       break;
     }
     delay(wait);
